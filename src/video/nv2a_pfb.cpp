ce47d325f4f07b569bcf0652cd00d83852a827f7
Matt Borgerson <contact@mborgerson.com>
2017-12-17 14:49:21 -0700

    Split up monster nv2a.c into files by component

---
+++ src/video/nv2a_pfb.cpp
@@ -0,0 +1,38 @@
+static uint64_t pfb_read(void *opaque,
+                         hwaddr addr, unsigned int size)
+{
+    NV2AState *d = opaque;
+
+    uint64_t r = 0;
+    switch (addr) {
+    case NV_PFB_CFG0:
+        /* 3-4 memory partitions. The debug bios checks this. */
+        r = 3;
+        break;
+    case NV_PFB_CSTATUS:
+        r = memory_region_size(d->vram);
+        break;
+    case NV_PFB_WBC:
+        r = 0; /* Flush not pending. */
+        break;
+    default:
+        r = d->pfb.regs[addr];
+        break;
+    }
+
+    reg_log_read(NV_PFB, addr, r);
+    return r;
+}
+static void pfb_write(void *opaque, hwaddr addr,
+                       uint64_t val, unsigned int size)
+{
+    NV2AState *d = opaque;
+
+    reg_log_write(NV_PFB, addr, val);
+
+    switch (addr) {
+    default:
+        d->pfb.regs[addr] = val;
+        break;
+    }
+}
