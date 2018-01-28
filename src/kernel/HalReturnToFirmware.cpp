7c0d8ca5eaf8dcd2affa26a73bdcebba5a4e711e
StrikerX3 <ivan.rober@gmail.com>
2018-01-28 14:49:14 -0200

    HalReturnToFirmware: log routine

--- src/kernel/HalReturnToFirmware.cpp
+++ src/kernel/HalReturnToFirmware.cpp
@@ -13,6 +13,27 @@ int Xbox::HalReturnToFirmware()
 	K_ENTER_STDCALL();
 	K_INIT_ARG(XboxTypes::FIRMWARE_REENTRY, Routine);
 
+	switch (Routine) {
+	case XboxTypes::HalHaltRoutine:
+		log_info("HalReturnToFirmware: Halt system\n");
+		break;
+	case XboxTypes::HalRebootRoutine:
+		log_info("HalReturnToFirmware: Reboot\n");
+		break;
+	case XboxTypes::HalQuickRebootRoutine:
+		log_info("HalReturnToFirmware: Quick reboot\n");
+		break;
+	case XboxTypes::HalKdRebootRoutine:
+		log_info("HalReturnToFirmware: Reboot to kernel debugger\n");
+		break;
+	case XboxTypes::HalFatalErrorRebootRoutine:
+		log_info("HalReturnToFirmware: Reboot due to fatal error\n");
+		break;
+	default:
+		log_info("HalReturnToFirmware: Unknown routine: %d\n", Routine);
+		break;
+	}
+
 	m_should_run = false;
 
 	K_EXIT();
